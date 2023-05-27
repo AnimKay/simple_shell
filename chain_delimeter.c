230b8fb4f7136dd7069376154d8c4af8ccce5a38
Lynx737 <89715457+Lynx737@users.noreply.github.com>
2023-05-27 00:20:08 +0000

    Update chain_delimeter.c

--- chain_delimeter.c
+++ chain_delimeter.c
@@ -106,34 +106,34 @@ int replace_alias(info_t *info)
  */
 int replace_vars(info_t *info)
 {
-	int i = 0;
+	int a = 0;
 	list_t *node;
 
-	for (i = 0; info->argv[i]; i++)
+	for (a = 0; info->argv[a]; a++)
 	{
-		if (info->argv[i][0] != '$' || !info->argv[i][1])
+		if (info->argv[a][0] != '$' || !info->argv[a][1])
 			continue;
 
-		if (!_strcmp(info->argv[i], "$?"))
+		if (!_strcmp(info->argv[a], "$?"))
 		{
-			replace_string(&(info->argv[i]),
+			replace_string(&(info->argv[a]),
 				_strdup(convert_number(info->status, 10, 0)));
 			continue;
 		}
-		if (!_strcmp(info->argv[i], "$$"))
+		if (!_strcmp(info->argv[a], "$$"))
 		{
-			replace_string(&(info->argv[i]),
+			replace_string(&(info->argv[a]),
 				_strdup(convert_number(getpid(), 10, 0)));
 			continue;
 		}
-		node = node_starts_with(info->env, &info->argv[i][1], '=');
+		node = node_starts_with(info->env, &info->argv[a][1], '=');
 		if (node)
 		{
-			replace_string(&(info->argv[i]),
+			replace_string(&(info->argv[a]),
 				_strdup(_strchr(node->str, '=') + 1));
 			continue;
 		}
-		replace_string(&info->argv[i], _strdup(""));
+		replace_string(&info->argv[a], _strdup(""));
 
 	}
 	return (0);
