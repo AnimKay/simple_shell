71772e46ecd062381aef14ea3ac2601914185fbd
Lynx737 <89715457+Lynx737@users.noreply.github.com>
2023-05-27 04:22:21 +0000

    Update string.c

--- string.c
+++ string.c
@@ -1,27 +1,27 @@
 #include "shell.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * _strlen - returns the length of the string
+ * @s: the string whose length to be check
  *
  * Return: integer length of string
  */
 int _strlen(char *s)
 {
-	int i = 0;
+	int a = 0;
 
 	if (!s)
 		return (0);
 
 	while (*s++)
-		i++;
-	return (i);
+		a++;
+	return (a);
 }
 
 /**
- * _strcmp - performs lexicogarphic comparison of two strangs.
- * @s1: the first strang
- * @s2: the second strang
+ * _strcmp - performs lexicogarphic comparison of two strngs.
+ * @s1: the first string
+ * @s2: the second string
  *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
