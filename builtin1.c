7081032c8a55efde125dd38636f5398e2a7f85be
Lynx737 <89715457+Lynx737@users.noreply.github.com>
2023-05-27 01:40:08 +0000

    Update builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -1,20 +1,20 @@
 #include "shell.h"
 
 /**
- * _myexit - exits the shell
- * @info: Structure containing potential arguments. Used to maintain
+ * _myexit - quits the shell
+ * @info: Structure containing potential arguments for maintaining
  *          constant function prototype.
  *  Return: exits with a given exit status
  *         (0) if info.argv[0] != "exit"
  */
 int _myexit(info_t *info)
 {
-	int exitcheck;
+	int quitcheck;
 
 	if (info->argv[1])  /* If there is an exit arguement */
 	{
-		exitcheck = _erratoi(info->argv[1]);
-		if (exitcheck == -1)
+		quitcheck = _erratoi(info->argv[1]);
+		if (quitcheck == -1)
 		{
 			info->status = 2;
 			print_error(info, "Illegal number: ");
